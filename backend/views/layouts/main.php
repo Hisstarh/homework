da7d72a169e09b473913980979709825521f1e33
KS <k.Suldzhenko@auchan.ru>
2018-05-14 17:07:34 +0700

    dump

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -37,6 +37,7 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'Склад', 'url' => ['/articles']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
---
+++ common/config/dumper.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RU00160012
+ * Date: 14.05.2018
+ * Time: 15:44
+ */
+/**
+ * Debug function
+ * d($var);
+ */
+function d($var,$caller=null)
+{
+    if(!isset($caller)){
+        $caller = array_shift(debug_backtrace(1));
+    }
+    echo '<code>File: '.$caller['file'].' / Line: '.$caller['line'].'</code>';
+    echo '<pre>';
+    yii\helpers\VarDumper::dump($var, 10, true);
+    echo '</pre>';
+}
+
+/**
+ * Debug function with die() after
+ * dd($var);
+ */
+function dd($var)
+{
+    $caller = array_shift(debug_backtrace(1));
+    d($var,$caller);
+    die();
+}
