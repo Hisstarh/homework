a4949531574e065e0ab4929a4b00cfc76d9bace2
ks <k.suldzhenko@auchan.ru>
2018-05-14 15:23:45 +0700

    база данных

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -28,10 +28,35 @@ class m130524_201442_init extends Migration
         $this->createTable('{{%articles}}', [
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull(),
+            'code' => $this->integer()->notNull()->unique(),
+            'place' => $this->string()->notNull(),
+            'sale_price' => $this->float()->notNull(),
+            'purchase_price' => $this->float()->notNull(),
+            'mark' => $this->string()->notNull(),
+            'body' => $this->string()->notNull(),
+            'drive' => $this->string()->notNull(),
             'model' => $this->string()->notNull(),
+            'description' => $this->string()->defaultValue(''),
+            'left_right' => $this->integer()->defaultValue(0),
+            'front_rear' => $this->integer()->defaultValue(0),
+            'top_bottom' => $this->integer()->defaultValue(0),
+            'code_manufacturer' => $this->string()->notNull(),
+            'optics' => $this->string()->notNull(),
+            'delevery_id' => $this->integer()->notNull(),
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'sit_id' =>$this->integer()->notNull(),
             'update_user' => $this->integer()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->createTable('{{%delevery}}', [
+            'id' => $this->primaryKey(),
+            'delevery' => $this->string()->notNull()->unique(),
+            'owner' => $this->string(32)->notNull(),
+            'delevery_date'=> $this->integer()->notNull(),
+            'sit_id' =>$this->integer()->notNull(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
@@ -44,6 +69,11 @@ class m130524_201442_init extends Migration
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+
+        $this->createIndex('FK_delevery_sit', '{{%delevery}}', 'sit_id');
+        $this->addForeignKey(
+            'FK_delevery_sit', '{{%delevery}}', 'sit_id', '{{%sit}}', 'id', 'SET NULL', 'CASCADE'
+        );
 
         $this->createIndex('FK_articles_sit', '{{%articles}}', 'sit_id');
         $this->addForeignKey(
@@ -64,7 +94,8 @@ class m130524_201442_init extends Migration
     public function down()
     {
         $this->dropTable('{{%user}}');
-        $this->dropTable('{{%Articles}}');
+        $this->dropTable('{{%articles}}');
         $this->dropTable('{{%sit}}');
+        $this->dropTable('{{%delevery}}');
     }
 }
